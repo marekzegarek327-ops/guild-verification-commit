0dfd0f9150ae9f6fd339dd39c12e10f848497ec1
marekzegarek327-ops <marekzegarek327@gmail.com>
2026-03-05 20:30:01 +0100

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:29:45.823Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T19:29:49.802Z
+Commit: Fix CSS styling bug on mobile devices
