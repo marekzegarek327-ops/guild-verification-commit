5ad58c667b792279301e65650594accdbbd7b9df
marekzegarek327-ops <marekzegarek327@gmail.com>
2026-03-05 20:29:38 +0100

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:24:39.473Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T19:29:26.731Z
+Commit: Fix memory leak in event handlers
