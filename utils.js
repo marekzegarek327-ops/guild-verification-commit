788539a9f27a9ae0225ad4f031b2a74126b87a7e
marekzegarek327-ops <marekzegarek327@gmail.com>
2026-03-05 20:24:49 +0100

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:24:30.808Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T19:24:37.764Z
+Commit: Resolve null pointer exception in user service
