57bd7d3821000c5bef1066e1166dfeb2eafedb86
marekzegarek327-ops <marekzegarek327@gmail.com>
2026-03-05 20:25:01 +0100

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:24:37.764Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T19:24:49.714Z
+Commit: Fix memory leak in event handlers
